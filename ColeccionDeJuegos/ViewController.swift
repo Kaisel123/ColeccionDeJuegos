f61ab7bf7bbc3798a81e800cf2b90d29eeb3a804
Bruno David Alejo Apaza <bruno.alejo@tecsup.edu.pe>
2021-05-17 10:19:27 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by David Alejo Apaza on 5/17/21.
+//  Copyright © 2021 David Alejo Apaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
